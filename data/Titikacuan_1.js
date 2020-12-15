8410fc55b22bc9ee5d09252925ed2c91bd50701e
Adelaiderani <76052140+Adelaiderani@users.noreply.github.com>
2020-12-16 00:09:47 +0700

    Add files via upload

---
+++ data/Titikacuan_1.js
@@ -0,0 +1,8 @@
+var json_Titikacuan_1 = {
+"type": "FeatureCollection",
+"name": "Titikacuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.409503336122583, -6.984110781995511 ] } }
+]
+}
